ecac16cb03ad3bd091b53424338210d0ca02df72
Nicole <chelseanbr@gmail.com>
2020-06-06 18:09:59 -0700

    add website code

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from collections import Counter
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return ""
+def dict_to_html(d):
+    return '<br>'.join('{0}: {1}'.format(k, d[k]) for k in sorted(d))
 
+# Home page
 @app.route('/')
 def index():
-    return ""
+    return render_template('jumbotron.html', title='Predict an Airbnb Rating')
+
+# My sentiment predictor app
+@app.route('/prediction', methods=['POST'])
+def show_pred():
+    text = str(request.form['user_input'])
+    return text
 
 
 if __name__ == '__main__':
